3d25e4df87f5944ae1cc611e90f4e98568c0452e
Luis Vargas <luisvargasr10@gmail.com>
2020-12-02 12:08:41 -0600

    Fisrt commit working with networking on SwiftUI

--- H4X0R-News/ContentView.swift
+++ H4X0R-News/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+        
     }
 }
 
@@ -19,3 +25,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    var id: String
+    var title: String
+}
+
+var posts = [
+    Post(id: "1", title: "Hola"),
+    Post(id: "2", title: "Hello"),
+    Post(id: "3", title: "Bounjour")
+]
