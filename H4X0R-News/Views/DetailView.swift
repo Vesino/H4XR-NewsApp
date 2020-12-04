0b049a68d5a38e885616818351c91c90474727e7
Luis Vargas <luisvargasr10@gmail.com>
2020-12-04 12:03:16 -0600

    Done, this is a version before to go with catalyst

--- H4X0R-News/Views/DetailView.swift
+++ H4X0R-News/Views/DetailView.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-import WebKit
+
 
 struct DetailView: View {
     
@@ -24,17 +24,3 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-struct WebView: UIViewRepresentable {
-    let urlString: String?
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-    }
-}
